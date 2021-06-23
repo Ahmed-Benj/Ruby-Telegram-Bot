768b8d0e18e7a074a2f561c2cdf7ed2e0602256c
ahmed BEN-JMII <ahmed.ben-jmii@talan.com>
2021-06-23 07:31:15 +0100

    fix linters

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'telegram/bot'
 
 token = '1799533455:AAEmVvoRD_K-0qAELHUJlphJDgroSo52ryM'
@@ -6,7 +8,8 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name}, Welcome to daily schedule bot, use commands /start")
+      bot.api.send_message(chat_id: message.chat.id,
+                           text: "Hello #{message.from.first_name}, Welcome to daily schedule bot, use commands /start")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Good Bye, #{message.from.first_name}")
     end

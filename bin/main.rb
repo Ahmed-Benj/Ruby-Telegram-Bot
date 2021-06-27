e324d4ab8cdf4d5e11c898687842a444c0a11b76
ahmed BEN-JMII <ahmed.ben-jmii@talan.com>
2021-06-27 18:10:20 +0100

    build Quote class

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 require_relative '../lib/bot'
+require_relative '../lib/quote'
 
-puts 'Welcome to Ruby Telegram Bot for Sports benefits'
-B = Telegrambot.new
-B.runbot
+# puts 'Welcome to Ruby Telegram Bot for Sports benefits'
+# B = Telegrambot.new
+# B.runbot
+
+quotes = Quote.new
+quote = quotes.getrandomquote
+puts (quote['text']).to_s
---
+++ lib/quote.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# A class to get random quote from json file
+class Quote
+  @quote = nil
+
+  def initialize
+    @quote = readquotesfile
+  end
+
+  def readquotesfile
+    file = File.read('C:\Users\abenjmii\Desktop\quotes.json')
+    JSON.parse(file)
+  end
+
+  def getrandomquote
+    @quote.sample
+  end
+end
